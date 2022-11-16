5f3ffb729a98c369465c2951b994b77d64dce6d4
omartheironman <58672497+omartheironman@users.noreply.github.com>
2022-11-15 22:37:37 -0500

    Update network.tf

--- deploy/modules/private-gke/network.tf
+++ deploy/modules/private-gke/network.tf
@@ -1,6 +1,6 @@
 
 resource "google_compute_global_address" "external_ip" {
-  project      = "mimetic-plate-368221" # Replace this with your service project ID in quotes
+  project      = "mimetic-plate-368221" # TODO: Template this to be passed by variables
   name         = "ipv6-address"
   address_type = "EXTERNAL"
   ip_version   = "IPV4"
