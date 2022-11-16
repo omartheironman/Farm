71c600d12311590310e803c500c3dcb978f0920e
omartheironman <58672497+omartheironman@users.noreply.github.com>
2022-11-15 22:37:08 -0500

    remove unused code

--- deploy/modules/private-gke/main.tf
+++ deploy/modules/private-gke/main.tf
@@ -42,37 +42,6 @@ resource "google_container_node_pool" "primary_nodes" {
   }
 }
 
-# resource "google_compute_global_forwarding_rule" "default" {
-#   name                  = "l7-xlb-forwarding-rule"
-#   ip_protocol           = "TCP"
-#   load_balancing_scheme = "EXTERNAL"
-#   port_range            = "80"
-#   target                = google_compute_target_http_proxy.default.id
-#   ip_address            = "34.120.245.36"
-# }
-
-# # http proxy
-# resource "google_compute_target_http_proxy" "default" {
-#   name     = "l7-xlb-target-http-proxy"
-#   url_map  = google_compute_url_map.default.id
-# }
-
-# # url map
-# resource "google_compute_url_map" "default" {
-#   name            = "l7-xlb-url-map"
-#   default_service = google_compute_backend_service.default.id
-# }
-
-# backend service with custom request and response headers
-# resource "google_compute_backend_service" "default" {
-#   name                    = "l7-xlb-backend-service"
-#   protocol                = "HTTP"
-#   port_name               = "service"
-#   load_balancing_scheme   = "EXTERNAL"
-#   timeout_sec             = 10
-#   enable_cdn              = true
-# }
-
 resource "kubernetes_service_v1" "grafanasvc" {
   metadata {
     name = "grafanasvc"
